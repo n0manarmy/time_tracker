92637bad91c6210c81420b6f4dce9724d2b96dbf
penn <penn@local>
2020-03-29 13:15:21 -0400

    implemented clock updates

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,44 @@ use gtk::prelude::*;
 
 use file_utils::FileUtils;
 use time_utils::TimeUtils;
+use std::env::args;
+use std::{thread, time};
 use std::rc::Rc;
-use glib::clone;
+use glib::{clone, Continue};
 use gtk::{
-    ApplicationWindow, Builder, Button, Label, Switch, Window, TextView, ListStore, TreeView, ScrolledWindow
+    ApplicationWindow, Builder, Button, Label, ListStore, TreeView, ScrolledWindow
 };
 
-use std::env::args;
+enum Message {
+    UpdateLabel(String),
+}
 
-fn build_ui(application: &gtk::Application) {
+
+fn build(application: &gtk::Application) {
     println!(
         "Major: {}, Minor: {}",
         gtk::get_major_version(),
         gtk::get_minor_version()
     );
+
+    let (sender, receiver) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+
     let glade_src = include_str!("../gui.glade");
     let builder = Builder::new_from_string(glade_src);
 
     let window: ApplicationWindow = builder.get_object("root").expect("Couldn't get window");
+    window.set_default_size(600,400);
     window.set_application(Some(application));
 
     // Buttons
     let button_time_in: Button = builder.get_object("button.time.in").expect("couldn't get time in button");
     let button_time_out: Button = builder.get_object("button.time.out").expect("couldn't get time in button");
-    let button_time_list: Button = builder.get_object("button.time.list").expect("couldn't get time in button");
+    // let button_time_list: Button = builder.get_object("button.time.list").expect("couldn't get time in button");
     
     // Labels
-    let label_last_clock_in: Label = builder.get_object("label.last.clock.in").expect("couldn't get label for last clock in");
-    let label_current_time: Label = builder.get_object("label.current.time").expect("couldn't get label for current time");
+    let label_last_clock_in: Label = builder.get_object("label.current.time").expect("couldn't get label for last clock in");
+    let current_time: Label = builder.get_object("current.time").expect("couldn't get label for current time");
+    current_time.set_text(&TimeUtils::get_time_stamp(""));
 
     // Views
     let scrolled_window: ScrolledWindow = builder.get_object("scrolled.window").expect("Couldn't load scrolled window");
@@ -57,9 +67,8 @@ fn build_ui(application: &gtk::Application) {
     add_columns(&model, &tree_view);    
 
     // Button functions and callbacks
-    button_time_in.connect_clicked(clone!(@weak list_store, @weak label_last_clock_in => move |_| {
+    button_time_in.connect_clicked(clone!(@weak list_store => move |_| {
         let time_stamp = TimeUtils::get_time_stamp("IN");
-        label_last_clock_in.set_text(&time_stamp);
         let col_indicies: [u32; 1] = [0];
         let values: [&dyn ToValue; 1] = [&time_stamp];
         list_store.set(&list_store.append(), &col_indicies, &values);
@@ -72,6 +81,23 @@ fn build_ui(application: &gtk::Application) {
         let values: [&dyn ToValue; 1] = [&time_stamp];
         list_store.set(&list_store.append(), &col_indicies, &values);
     }));
+
+    thread::spawn(move || {
+        loop {
+            thread::sleep(time::Duration::from_secs(1));
+            let _ = sender.send(Message::UpdateLabel(TimeUtils::get_time_stamp("")));
+        }
+    });
+
+    let current_time_clone = current_time.clone();
+    
+    receiver.attach(None, move |msg| {
+        match msg {
+            Message::UpdateLabel(text) => current_time_clone.set_text(text.as_str()),
+        }
+
+        glib::Continue(true)
+    });
 
     window.show_all();
 }
@@ -92,6 +118,7 @@ fn add_columns(model: &Rc<ListStore>, tree_view: &TreeView) {
         let column = gtk::TreeViewColumn::new();
         column.pack_start(&renderer, true);
         column.set_title("Log");
+        column.set_alignment(0.5);
         column.add_attribute(&renderer, "text", 0);
         tree_view.append_column(&column);
     }
@@ -105,8 +132,10 @@ fn main() {
     .expect("Initialization failed...");
 
     application.connect_activate(|app| {
-        build_ui(app);
+        build(app);
     });
+
+    //init data store for times
 
     application.run(&args().collect::<Vec<_>>());
 }
